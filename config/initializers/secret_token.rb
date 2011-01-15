34e3494c69a9a3aeefb0fa42a8969e60bb0379cd
jnwelzel <jnwelzel@gmail.com>
2011-01-15 00:47:59 -0200

    Initial commit manolo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipebook::Application.config.secret_token = '15fef18a7cd8e09853d6898b7532729082c8c51a7190e5a7cdd44ab9b46dbe9c0e267f31d96ac506ad79367e52197800960d2fd42798ab2ed6422d4197d33d87'
