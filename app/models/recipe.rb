73142de5a15374cf410bcb04f53bea649569ddb8
jnwelzel <jnwelzel@gmail.com>
2011-01-18 23:00:59 -0200

    Final touches before adding authentication, here goes nothin

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,7 @@ class Recipe < ActiveRecord::Base
                    :length => {:minimum => 3, :maximum => 60}
   validates :ingredients, :presence => true,
                    :length => {:minimum => 3, :maximum => 400}
+  #rails generate migration add_instructions_to_recipes instructions:string
   validates :instructions, :presence => true,
-                   :length => {:minimum => 3, :maximum => 2048}                   
+                   :length => {:minimum => 3, :maximum => 2048}                  
 end
